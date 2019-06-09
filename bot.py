c53bbeaa77933cc34070da697bdac5eeef0502ac
cactusan <cactus-2015@mail.ru>
2019-06-09 22:37:18 +0300

    Списки слов ответов и вопросов

--- bot.py
+++ bot.py
@@ -3,26 +3,24 @@ import random
 import bs4 as bs4
 import requests
 
+import lists
+
 class Bot:
 	"""
 	Отвечает на сообщения.
 	"""
 	def __init__(self, user_id):
 		self.USER_ID = user_id
-		self.HELLO = ["ПРИВЕТ", "ХАЙ", "ЗДРАВСТВУЙ"]
-		self.HELLO_ANSWER = ["Привет!)", "Хай)", "Здравствуй))", "Hello my friend!"]
-		self.BYE = ["Пока-пока!))", "До встречи!", "Увидимся)", "Славься хлеб!"]
-		self.CREATOR = ["СОЗДАТЕЛЬ", "АВТОР", "РАЗРАБОТЧИК", "КТО ТВОЙ СОЗДАТЕЛЬ"]
 
 	def new_message(self, message):
 		# Привет
-		if message.upper() in self.HELLO:
-			return random.choice(self.HELLO_ANSWER)
+		if message.upper() in lists.HELLO_Q:
+			return random.choice(lists.HELLO_A)
 		# Создатель
-		elif message.upper() in self.CREATOR:
+		elif message.upper() in lists.CREATOR_Q:
 			return "https://vk.com/cact_us"
-		elif message.upper() == "ПОКА":
-			return random.choice(self.BYE)
+		elif message.upper() in lists.BYE_Q:
+			return random.choice(lists.BYE_A)
 		# Неизвестное сообщение
 		else:
 			return "Я вас не понимаю.("
