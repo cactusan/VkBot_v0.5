142c90957592cee482343579d0d765e0fe65c6d5
cactusan <cactus-2015@mail.ru>
2019-08-10 11:54:57 +0300

    1

--- bot.py
+++ bot.py
@@ -1,26 +1,36 @@
 import random
-
-import bs4 as bs4
-import requests
+import vk_requests
 
 import lists
 
-class Bot:
-	"""
-	Отвечает на сообщения.
-	"""
-	def __init__(self, user_id):
+class Bot(object):
+	""" Work with user messages """
+	
+	def __init__(self, user_id, token):
+		self.APP_TOKEN = token
 		self.USER_ID = user_id
-
+		self.user_name = self.get_user_info()
+		
 	def new_message(self, message):
-		# Привет
-		if message.upper() in lists.HELLO_Q:
-			return random.choice(lists.HELLO_A)
-		# Создатель
-		elif message.upper() in lists.CREATOR_Q:
-			return "https://vk.com/cact_us"
-		elif message.upper() in lists.BYE_Q:
-			return random.choice(lists.BYE_A)
-		# Неизвестное сообщение
-		else:
-			return "Я вас не понимаю.("
+		""" Creating a new message """
+		try:
+			if message.upper() in lists.HELLO_Q:
+				return random.choice(lists.HELLO_A) + self.user_name + random.choice(lists.SYMBOLS)
+			elif message.upper() in lists.CREATOR_Q:
+				return "https://vk.com/cact_us"
+			elif message.upper() in lists.BYE_Q:
+				return random.choice(lists.BYE_A)
+			else:
+				return "Я вас не понимаю.("
+		except Exception as e:
+			print("[New message] :: ", e)
+	
+	def get_user_info(self):
+		""" Getting user information """
+
+		try:
+			api = vk_requests.create_api(service_token=self.APP_TOKEN)
+			user = api.users.get(user_ids=self.USER_ID)
+			return user[0]['first_name']
+		except Exception as e:
+			print("[Get user info] :: ", e)
--- main.py
+++ main.py
@@ -1,47 +1,59 @@
 import random as rnd
-
-import vk_api
 import requests
+
 from vk_api.longpoll import VkLongPoll, VkEventType
+import vk_api
 
 from bot import Bot
 from weather import Weather
 
-# Авторизация для сообщества
-vk = vk_api.VkApi(token="TOKEN HERE")
-longpoll = VkLongPoll(vk)
+class BotMainLoop(object):
+	""" Bot main class """
 
-# Функция отправки сообшений
-def write_msg(user_id, random_id, message):
-	vk.method("messages.send", {"user_id": user_id, "random_id": random_id, "message": message})
+	def __init__(self):
+		self.app_token = ""
+		self.vk = vk_api.VkApi(token=self.app_token)
+		self.longpoll = VkLongPoll(self.vk)
+		self.loop()
+
+	def write_msg(self, user_id, random_id, message):
+		""" Message sending function"""
+
+		self.vk.method("messages.send", {"user_id": user_id, "random_id": random_id, "message": message})
+
+	def loop(self):
+		""" Main bot cycle """
+
+		print("[The bot is running]")
+		exit = False
+		while not exit:
+			try:
+				# Прием сообшений
+				for event in self.longpoll.listen():
+					if event.type == VkEventType.MESSAGE_NEW:
+						if event.to_me:
+							bot = Bot(event.user_id, self.app_token)
+							random_id = rnd.randrange(1, 500, 1)
+							text = event.text.split(" ")
+							# Если пользователь хочет узнать погоду.
+							if text[0].upper() == "ПОГОДА":
+								bot = Weather(event.user_id)
+								try:
+									city_id = bot.find_city(text[1])
+									city = bot.upper_name(text[1])
+									message = bot.find_out_the_weather(city_id, city)
+									self.write_msg(event.user_id, random_id, message)
+								except Exception as e:
+									print("[Incorrect input] :: ", e)
+									self.write_msg(event.user_id, random_id, "Не могу найти информацию по Вашему запросу.")
+							# Общение с ботом
+							else:
+								self.write_msg(event.user_id, random_id, bot.new_message(event.text))
+			except:
+				print("[The bot is off]")
+				exit = True
 
 
-print("[The bot is running]")
 
-exit = False
-while not exit:
-	try:
-		# Прием сообшений
-		for event in longpoll.listen():
-			if event.type == VkEventType.MESSAGE_NEW:
-				if event.to_me:
-					bot = Bot(event.user_id)
-					random_id = rnd.randrange(1, 500, 1)
-					text = event.text.split(" ")
-					# Если пользователь хочет узнать погоду.
-					if text[0].upper() == "ПОГОДА":
-						bot = Weather(event.user_id)
-						try:
-							city_id = bot.find_city(text[1])
-							city = bot.upper_name(text[1])
-							message = bot.find_out_the_weather(city_id, city)
-							write_msg(event.user_id, random_id, message)
-						except Exception as e:
-							print("[Incorrect input] :: ", e)
-							write_msg(event.user_id, random_id, "Не могу найти информацию.")
-					# Общение с ботом
-					else:
-						write_msg(event.user_id, random_id, bot.new_message(event.text))
-	except:
-		print("[The bot is off]")
-		exit = True
+if __name__ == "__main__":
+	BotMainLoop()
