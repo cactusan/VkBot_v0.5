edf6cb52523fc0345d1e82feb9be5bb3155ce31b
cactusan <cactus-2015@mail.ru>
2019-08-11 18:07:12 +0300

    Переработка комманд

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-import random as rnd
+import random
 import requests
 import sys
 
 from vk_api.longpoll import VkLongPoll, VkEventType
 import vk_api
+import vk_requests
 
 sys.path.insert(1, r'modules')
-from bot import Bot
 from weather import Weather
 import cities
+import lists
 
 class BotMainLoop(object):
 	""" Bot main class """
-
 	def __init__(self):
 		self.app_token = "00b73157edcc1908a48d21a259762638b7cec649e01375e2dc70f5dc5a44f2ec88c6b80d66c85df03343f"
 		self.vk = vk_api.VkApi(token=self.app_token)
@@ -21,12 +21,19 @@ class BotMainLoop(object):
 
 	def write_msg(self, user_id, random_id, message):
 		""" Message sending function"""
-
 		self.vk.method("messages.send", {"user_id": user_id, "random_id": random_id, "message": message})
+
+	def get_user_info(self, id):
+		""" Getting user information """
+		try:
+			api = vk_requests.create_api(service_token=self.app_token)
+			user = api.users.get(user_ids=id)
+			return user[0]['first_name']
+		except Exception as e:
+			print("[Get user info] :: ", e)
 
 	def loop(self):
 		""" Main bot cycle """
-
 		print("[The bot is running]")
 		exit = False
 		while not exit:
@@ -35,27 +42,37 @@ class BotMainLoop(object):
 				for event in self.longpoll.listen():
 					if event.type == VkEventType.MESSAGE_NEW:
 						if event.to_me:
-							bot = Bot(event.user_id, self.app_token)
-							random_id = rnd.randrange(1, 500, 1)
-							text = event.text.split(" ")
+							random_id = random.randrange(1, 500, 1)
+							text = event.text
+							command = event.text.split(" ")
+							if text.upper() in lists.HELLO_Q:
+								user_name = self.get_user_info(event.user_id)
+								message = random.choice(lists.HELLO_A) + user_name + random.choice(lists.SYMBOLS)
+								self.write_msg(event.user_id, random_id, message)
+							elif text.upper() in lists.CREATOR_Q:
+								message = "https://vk.com/cact_us"
+								self.write_msg(event.user_id, random_id, message)
+							elif text.upper() in lists.BYE_Q:
+								message = random.choice(lists.BYE_A)
+								self.write_msg(event.user_id, random_id, message)
 							# Если пользователь хочет узнать текущую погоду.
-							if text[0].upper() == "ПОГОДА":
+							elif command[0].upper() == "ПОГОДА":
 								weather_mod = Weather()
 								try:
-									city_id = weather_mod.searchCity(text[1])
-									city = text[1].capitalize()
+									city_id = weather_mod.searchCity(command[1])
+									city = command[1].capitalize()
 									message = weather_mod.current_weather(city_id, city)
 									self.write_msg(event.user_id, random_id, message)
 								except Exception as e:
 									print("[Incorrect city input (current weather)] :: ", e)
 									self.write_msg(event.user_id, random_id, "Не могу найти информацию по Вашему запросу.")
 							# Если пользователь хочет узнать прогноз погоды.
-							elif text[0].upper() == "ПРОГНОЗ":
+							elif command[0].upper() == "ПРОГНОЗ":
 								weather_mod = Weather()
 								try:
-									city_id = weather_mod.searchCity(text[1])
-									city = text[1].capitalize()
-									days = int(text[2])
+									city_id = weather_mod.searchCity(command[1])
+									city = command[1].capitalize()
+									days = int(command[2])
 									message = weather_mod.weather_forecast(city_id, city, days)
 									# self.write_msg(event.user_id, random_id, message)
 									self.write_msg(event.user_id, random_id, "Данная функция находится в разработке.")
@@ -64,7 +81,8 @@ class BotMainLoop(object):
 									self.write_msg(event.user_id, random_id, "Не могу найти информацию по Вашему запросу.")
 							# Общение с ботом
 							else:
-								self.write_msg(event.user_id, random_id, bot.new_message(event.text))
+								message = "Я вас не понимаю.("
+								self.write_msg(event.user_id, random_id, message)
 			except:
 				print("[The bot is off]")
 				exit = True
--- modules/cities.py
+++ modules/cities.py
@@ -1,7 +1,50 @@
-used = []
-unused = []
+"""
+----------------------------------------ТЗ----------------------------------------
 
-with open(r"modules\RussiaCities.txt", "r") as file:
-    for text in file:
-        text = text[:-1]
-        unused.append(text)
+-Пользователь пишет "Поиграем в города"
+-Бот отвечает и предлагает пользователю первому назвать город
+-Бот проверяет нет ли этого названия в списке названных городов
+    -Если город уже был назван, то Бот сообщает об этом
+    -Ждет название города
+-Бот проверяет город в списке не названных городов
+    -Если такого города в России нет, Бот сообщает об этом
+    -Если название верно, то добавляет город в список использованных городов
+    -Ждет название города
+-Бот называет город из списка не названных городов
+    -На последнюю букву названного пользователем города Бот в списке не названных городов находит "случайный" подходящий город
+        (список каждый раз перемешивается) и говорит пользователю
+    -Если название верно, то добавляет город в список использованных городов
+    -Ждет название города
+-Бот ждет команды "Стоп" или название города
+"""
+import random
+
+
+
+class Game(object):
+    """ City's main game class """
+    def __init__(self):
+        self.inaccessible_pool = []
+        self.available_pool = []
+
+    def readCityNames(self):
+        """ Reading city names from file """
+        with open(r"modules\RussiaCities.txt", "r") as file:
+            for city in file:
+                city = city[:-1]
+                self.available_pool.append(city)
+
+    def checkCityName(self, city):
+        if city in self.inaccessible_pool:
+            return 100                      # Название находиться в недоступном пуле
+        else:
+            if city in self.available_pool:
+                return 101                  # Название доступно для использования
+            else:
+                return 102                  # Такого города в России не существует
+
+    def giveAnAnswer(self):
+        pass
+
+    def addCityToUsedPool(self, city):
+        self.inaccessible_pool.append(city)
